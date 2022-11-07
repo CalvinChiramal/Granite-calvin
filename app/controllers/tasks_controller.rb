6ba1b5dc5bec6f9b955524fcebed6fa03ec5c044
Calvin Chiramal <calvin.chiramal@bigbinary.com>
2022-11-07 15:35:42 +0530

    Added show page for a task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,13 +3,18 @@
 class TasksController < ApplicationController
   def index
     tasks = Task.all
-    render status: :ok, json: { tasks: tasks }
+    respond_with_json({ tasks: tasks })
   end
 
   def create
     task = Task.new(task_params)
     task.save!
     respond_with_success(t("successfully_created"))
+  end
+
+  def show
+    task = Task.find_by!(slug: params[:slug])
+    respond_with_json({ task: task })
   end
 
   private
